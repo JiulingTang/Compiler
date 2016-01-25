06b2e2f7b9db131a330a92bdbcd16f820adac6d0
Jiuling Tang <jiulingtang@gmai.com>
2016-01-25 17:43:43 -0500

    Update LexicalAnylazer.java

--- LexicalAnylazer/LexicalAnylazer.java
+++ LexicalAnylazer/LexicalAnylazer.java
@@ -1,13 +1,69 @@
 package LexicalAnylazer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 public class LexicalAnylazer {
 	private Hashtable<ArrayList,Integer> table; //Transition Table
-	private void addTrans(int a,char b,int c)
+	private String r="";
+	private int id;
+	private int state;
+	private Set<Character> chSet; //Set of valid Characters
+	private Hashtable<Integer,String> fStateMap;
+	private void addTrans(int cState,char ch,int nState)
 	{
 		ArrayList l=new ArrayList();
+		l.add(cState);
+		l.add(ch);
+		table.put(l, nState);
+		
+	}
+	
+	private boolean valid(char c)
+	{
+		return chSet.contains(c);
+	}
+	
+	public LexicalAnylazer()
+	{
+		
+		chSet=new HashSet<Character>();
+		/*Add valid Characters*/
+		for (char c='a';c<='z';c++)
+			chSet.add(c);
+		for (char c='A';c<='Z';c++)
+			chSet.add(c);
+		for (char c='0';c<='9';c++)
+			chSet.add(c);
+		chSet.add('=');
+		chSet.add('<');
+		chSet.add('>');
+		chSet.add('+');
+		chSet.add('-');
+		chSet.add('*');
+		chSet.add('/');
+		chSet.add(';');
+		chSet.add(',');
+		chSet.add('.');
+		chSet.add('*');
+		chSet.add('(');
+		chSet.add(')');
+		chSet.add('[');
+		chSet.add(']');
+		chSet.add('{');
+		chSet.add('}');
+		chSet.add(' ');
+		chSet.add('\r');
+		chSet.add('\n');
+		
+		
+		
+		table=new Hashtable<ArrayList,Integer>();
+		this.addTrans(0, ' ', 0);
+		this.addTrans(0, '\r', 0);
+		this.addTrans(0, '\n', 0);
 		/*The transition for letter*/
 		this.addTrans(0, 'a', 1);
 		this.addTrans(1, 'a', 2);
@@ -20,17 +76,48 @@ public class LexicalAnylazer {
 		this.addTrans(3, '0', 2);
 		this.addTrans(3, '1', 2);
 		
+		state=0;
 	}
-	public LexicalAnylazer()
+	
+	private int trans(int cState,char ch)
 	{
-		table=new Hashtable<ArrayList,Integer>();
 		ArrayList l=new ArrayList();
+		l.add(cState);
+		l.add(ch);
+		if (!table.contains(l))
+			return -1;
+		else
+			return table.get(l);
+		
+	}
+	
+	private boolean finalState(int st)
+	{
+		return fStateMap.contains(st);
 	}
 	
 	public String nextToken()
 	{
 		
-		state=trans(state,)
+		String s="";
+		while (id!=r.length())
+		{
+			char c=r.charAt(id++);
+			if (!valid(c)) //If invalid Character, skip it
+			continue;
+			state=trans(state,c);
+			if (state==-1)
+			{
+				state=0;
+				id--;
+			}
+			if (finalState(state))
+			{
+				;
+			}
+		}
+	
+		return s;
 	}
 	
 }
